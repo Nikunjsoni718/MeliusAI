cb2f3a0cb44aab7f704eb5c25f20999397516029
Nikunj soni <sanjusoni15699@gmail.com>
2026-06-30 21:48:04 +0530

    removing the footer tab completely

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { SessionRouteGuard } from '@/components/auth/session-route-guard';
 import { LandingPage } from '@/components/marketing/landing-page';
-import { SiteFooter } from '@/components/layout/site-footer';
 
 export default function Page() {
   return (
     <SessionRouteGuard>
       <LandingPage />
-      <SiteFooter />
     </SessionRouteGuard>
   );
 }
